277373bfabdd34c2e204acd908fb843011ed8e10
Francesco Zanini <francesco.zanini@znp-group.com>
2015-02-20 21:15:56 +0100

    Added "Altro" background.

---
+++ static/images/m/altro/orig/furgone/convert.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os, glob, subprocess, shlex
+
+RES = {
+        'large_2x': '2242x',
+        'large': '1121x',
+        'medium_2x': '1546x',
+        'medium': '773x',
+        'small_2x': '1246x',
+        'small': '623x'
+        }
+
+ORIG_DIR = os.path.dirname(__file__)
+NAME = os.path.basename(os.path.realpath(ORIG_DIR))
+DEST_DIR = os.path.relpath(os.path.join(ORIG_DIR, '..', '..', NAME))
+
+images = glob.glob(os.path.join(ORIG_DIR, '*.jpg'))
+images = map(lambda x: os.path.basename(x), images)
+
+def basename_ext(filename):
+    pieces = filename.split('.')
+    return '.'.join(pieces[:-1]), pieces[-1]
+
+if os.path.exists(DEST_DIR):
+    if not os.path.isdir(DEST_DIR):
+        raise ValueError('Destination directory already exists and it\'s a file')
+else:
+    os.mkdir(DEST_DIR)
+
+plist = []
+
+for image in images:
+    basename, ext = basename_ext(image)
+    image_src = os.path.relpath(os.path.join(ORIG_DIR, image), os.getcwd())
+    for rid, rdata in RES.items():
+        destination = os.path.join(DEST_DIR, '{}_{}.{}'.format(basename, rid, ext))
+        cmd = "convert -quality 90 -resize {} {} {}".format(rdata, image_src, destination)
+        print "Running {}".format(cmd)
+        cmd = shlex.split(cmd)
+        p = subprocess.Popen(cmd)
+        plist.append(p)
+
+for p in plist:
+    p.wait()
+
+print 'Finished'
