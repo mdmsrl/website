2eea7e410e65717f1402c7ead5e8acfd02a69c12
Francesco Zanini <francesco@zanini.me>
2019-01-23 09:49:10 +0100

    Remove outdated files

--- scripts/sync_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-aws s3 sync . s3://dev.machinedesignmn.com --size-only --delete --exclude '.git/*' --exclude 'static/.sass-cache/*' --exclude '.gitignore' --exclude 'README.md' --exclude 'scripts/*'
--- scripts/sync_prod.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-aws s3 sync . s3://www.mdm-srl.com --size-only --delete --exclude '.git/*' --exclude 'static/.sass-cache/*' --exclude '.gitignore' --exclude 'README.md'
